877d466cc0c43c87c7929ab45ae579ec27d84e66
ap.tomchik <>
2024-04-18 18:02:45 +0300

    do task 3 less 9

--- 9/task_2.py
+++ 9/task_2.py
@@ -31,17 +31,48 @@
 # Реализовать без подключения новых модулей и сторонних библиотек.
 
 
-import datetime
 import time
 def func_log(func):
     def wrapper(*args):
-        func(*args)
-        b = print(f"{func.__name__} вызвана {time.strftime('%d.%m %H:%M:%S')}")
-        return b
+        if len(args) == 0 or args[0] == 'log.txt':
+            b = f"{func(*args)}\n{func.__name__} вызвана {time.strftime('%d.%m %H:%M:%S')}\n"
+            a = 'C:\\Users\\ap.tomchik\PycharmProjects\Автотестирование2023\9\\test_file\\log.txt'
+            f = open(a, mode='a', encoding='utf-8')
+            f.write(b)
+            f.close()
+        else:
+            b = f"{func(*args)}\n{func.__name__} вызвана {time.strftime('%d.%m %H:%M:%S')}\n"
+            a = 'C:\\Users\\ap.tomchik\PycharmProjects\Автотестирование2023\9\\test_file\\func2.txt'
+            f = open(a, mode='a', encoding='utf-8')
+            f.write(b)
+            f.close()
     return wrapper
-
 @func_log
 def my_func1(file_log='log.txt'):
-    print(f'в {file_log} текст:')
+    res = (f'в {file_log} текст:')
+    return res
 
 my_func1('func2.txt')
+
+# import time
+# def func_log(func):
+#     def wrapper(*args):
+#         if len(args) == 0 or args[0] == 'log.txt':
+#             b = f"{func(*args)}\n{func.__name__} вызвана {time.strftime('%d.%m %H:%M:%S')}\n"
+#             a = 'C:\\Users\\ap.tomchik\PycharmProjects\Автотестирование2023\9\\test_file\\log.txt'
+#             f = open(a, mode='a', encoding='utf-8')
+#             f.write(b)
+#             f.close()
+#         else:
+#             b = f"{func(*args)}\n{func.__name__} вызвана {time.strftime('%d.%m %H:%M:%S')}\n"
+#             a = 'C:\\Users\\ap.tomchik\PycharmProjects\Автотестирование2023\9\\test_file\\func2.txt'
+#             f = open(a, mode='a', encoding='utf-8')
+#             f.write(b)
+#             f.close()
+#     return wrapper
+# @func_log
+# def my_func1(file_log='log.txt'):
+#     res = (f'в {file_log} текст:')
+#     return res
+
+# my_func1()
