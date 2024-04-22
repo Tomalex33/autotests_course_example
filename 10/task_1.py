70e580e0a5aa3b63b47a94ca20e79b475cc9e4bf
Tomalex33 <tom2cyn@mail.ru>
2024-04-22 21:08:25 +0300

    do task 1 lesson 10

--- 10/task_1.py
+++ 10/task_1.py
@@ -12,46 +12,25 @@
 # dxqebbukr jg
 # aym jpvezfqexlv
 # iuy qnikkgxvxfxtxv
+
 import random
 
-# import random
-#
-# letter_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-#
-# def generate_random_name():
-#     word = ''
-#     for i in letter_list:
-#         yield random.choice(letter_list) + word
-#         print(word, 'добавляем букву')
-#         word = word + i
-#
-# gen = generate_random_name()
-#
-# for p in gen:
-#     print(p)
-
-# new_word = ''
-# s = random.choice(letter_list)
-# new_word = new_word + s
-# print(new_word)
-
-# print(next(gen))
-# print(next(gen))
-# print(next(gen))
-# print(next(gen))
-
-# def my_range(start, end=None):
-#     if end is None:
-#         start, end = 0, start
-#         yield start
-#         start = start + 1
-#         if start == end:
-#             raise StopIteration("end")
-#
-#
 letter_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-for i in random.choice(letter_list):
-    print(i)
-#     s = random.choice(letter_list)
-# print(s)
+def generate_random_name():
+    si = ''
+    k = random.randint(1, 15)
+    b = random.sample(letter_list, k)
+    for i in b:
+        si = si + i
+    si1 = ''
+    k1 = random.randint(1, 15)
+    b1 = random.sample(letter_list, k1)
+    for u in b1:
+        si1 = si1 + u
+
+    return f'{si} {si1}'
+
+print(generate_random_name())
+
+
