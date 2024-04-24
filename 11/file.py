d9480f490a2627398d1c805e46b8de0bd518780b
Tomalex33 <tom2cyn@mail.ru>
2024-04-24 21:51:43 +0300

    lesson 11

--- 11/file.py
+++ 11/file.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from time import sleep
-from selenium.webdriver import Keys
+from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.common.by import By
 
 sbis_site = 'https://test.sbis.ru/'
@@ -10,6 +10,7 @@ driver = webdriver.Chrome()
 try:
     driver.get(sbis_site)
     sleep(2)
+    driver.maximize_window()
     assert driver.current_url == sbis_site, 'Не верно открыт сайт'
     assert driver.title == sbis_title, 'Не верный заголовок'
     tabs = driver.find_elements(By.CSS_SELECTOR, '.sbisru-Header__menu-item')
@@ -22,11 +23,24 @@ try:
     assert start_btn.is_displayed()
     start_btn.click()
     driver.switch_to.window(driver.window_handles[1])
-    sleep(2)
+    sleep(3)
     login = driver.find_element(By.CSS_SELECTOR, '.controls-Field.js-controls-Field.controls-InputBase__nativeField.controls-InputBase__nativeField_caretFilled.controls-InputBase__nativeField_caretFilled_theme_default')
-    login.send_keys('my_login', Keys.ENTER)
-    assert login.get_attribute('value') == 'my_login'
+    login.send_keys('тест', Keys.ENTER)
+    sleep(2)
+    # assert login.get_attribute('value') == 'my_login'
     password = driver.find_element(By.CSS_SELECTOR, '.controls-Password__nativeField_caretFilled_theme_default')
-    password.send_keys('password')
+    password.send_keys('тест123', Keys.ENTER)
+    sleep(4)
+    news = driver.find_elements(By.CSS_SELECTOR, '[data-qa="list"]')
+    action_chains = ActionChains(driver)
+    action_chains.move_to_element(news[1])
+    action_chains.context_click(news[1])
+    action_chains.perform()
+    sleep(3)
+    context_menu = driver.find_element(By.CSS_SELECTOR, '[templatename="Controls/menu:Popup"]')
+    context_menu.is_displayed()
+    sleep(4)
 finally:
     driver.quit()
+    driver.close()
+    login.location_once_scrolled_into_view
